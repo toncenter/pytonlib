e351eba1cff38e6ecaf29e45a691aed2025a015e
K-Dimentional Tree <kdimentionaltree@gmail.com>
2022-06-03 22:13:53 +0300

    Tests updated

--- tests/pytonlib/test_tonlibclient.py
+++ tests/pytonlib/test_tonlibclient.py
@@ -45,53 +45,90 @@ async def tonlib_client(tonlib_config, ton_keystore, ls_index):
 # tests
 @pytest.mark.asyncio
 async def test_get_masterchain_info(tonlib_client: TonlibClient):
-    res = await tonlib_client.get_masterchain_info()
-    assert res['@type'] == 'blocks.masterchainInfo'
-
-    await tonlib_client.close()
+    exception = None
+    try:
+        res = await tonlib_client.get_masterchain_info()
+        assert res['@type'] == 'blocks.masterchainInfo'
+    except Exception as ee:
+        exception = ee
+    finally:    
+        await tonlib_client.close()
+    assert exception is None
 
 
 @pytest.mark.asyncio
 async def test_get_block_header(tonlib_client: TonlibClient):
-    masterchain_block = await tonlib_client.get_masterchain_info()
-    res = await tonlib_client.get_block_header(**masterchain_block['last'])
-    assert res['@type'] == 'blocks.header'
-
-    await tonlib_client.close()
+    exception = None
+    try:
+        masterchain_block = await tonlib_client.get_masterchain_info()
+        res = await tonlib_client.get_block_header(**masterchain_block['last'])
+        assert res['@type'] == 'blocks.header'
+    except Exception as ee:
+        exception = ee
+    finally:    
+        await tonlib_client.close()
+    assert exception is None
 
 
 @pytest.mark.asyncio
 async def test_get_shards(tonlib_client: TonlibClient):
-    masterchain_info = await tonlib_client.get_masterchain_info()
-    shards = await tonlib_client.get_shards(master_seqno=masterchain_info['last']['seqno'])
-    assert shards['@type'] == 'blocks.shards'
-
-    await tonlib_client.close()
+    exception = None
+    try:
+        masterchain_info = await tonlib_client.get_masterchain_info()
+        shards = await tonlib_client.get_shards(master_seqno=masterchain_info['last']['seqno'])
+        assert shards['@type'] == 'blocks.shards'
+    except Exception as ee:
+        exception = ee
+    finally:    
+        await tonlib_client.close()
+    assert exception is None
 
 
 @pytest.mark.asyncio
 async def test_get_transactions(tonlib_client: TonlibClient):
-    masterchain_info = await tonlib_client.get_masterchain_info()
+    exception = None
+    try:
+        masterchain_info = await tonlib_client.get_masterchain_info()
 
-    txs = await tonlib_client.get_block_transactions(**masterchain_info['last'], count=10)
-    assert txs['@type'] == 'blocks.transactions'
+        txs = await tonlib_client.get_block_transactions(**masterchain_info['last'], count=10)
+        assert txs['@type'] == 'blocks.transactions'
 
-    tx = await tonlib_client.get_transactions(**txs['transactions'][0], limit=1)
-    assert tx[0]['@type'] == 'raw.transaction'
-    
-    await tonlib_client.close()
+        tx = await tonlib_client.get_transactions(**txs['transactions'][0], limit=1)
+        assert tx[0]['@type'] == 'raw.transaction'
+    except Exception as ee:
+        exception = ee
+    finally:    
+        await tonlib_client.close()
+    assert exception is None
+
+
+@pytest.mark.asyncio
+async def test_correct_close(tonlib_client: TonlibClient):
+    exception = None
+    try:
+        masterchain_info = await tonlib_client.get_masterchain_info()
+        raise RuntimeError('Test error')
+    except Exception as ee:
+        exception = ee
+    finally:
+        await tonlib_client.close()
+    assert exception.args == ('Test error',)
 
 
 def test_sync_code(tonlib_config, ton_keystore, ls_index):
     async def main():
         loop = asyncio.get_running_loop()
-
-        client = TonlibClient(ls_index=ls_index,
-                              config=tonlib_config,
-                              keystore=ton_keystore,
-                              loop=loop,
-                              verbosity_level=0)
-        await client.init()
-        await client.close()
-
+        exception = None
+        try:
+            client = TonlibClient(ls_index=ls_index,
+                                config=tonlib_config,
+                                keystore=ton_keystore,
+                                loop=loop,
+                                verbosity_level=0)
+            await client.init()    
+        except Exception as ee:
+            exception = ee
+        finally:    
+            await client.close()
+        assert exception is None
     asyncio.run(main())
