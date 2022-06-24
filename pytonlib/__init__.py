bf5ba6399cd9d2dd18ab212a30c39a3b527dd583
dungeon-master-666 <dungeon666master@protonmail.com>
2022-06-24 22:39:41 +0200

    Export TonlibException

--- pytonlib/__init__.py
+++ pytonlib/__init__.py
@@ -1,2 +1,2 @@
 from pytonlib.client import TonlibClient
-from pytonlib.tonlibjson import TonlibNoResponse, TonlibError, LiteServerTimeout, BlockNotFound, ExternalMessageNotAccepted, BlockDeleted
+from pytonlib.tonlibjson import TonlibException, TonlibNoResponse, TonlibError, LiteServerTimeout, BlockNotFound, ExternalMessageNotAccepted, BlockDeleted
