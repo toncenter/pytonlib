6ebfebe65ffe80453a7d05c3fd76ec30486d64ef
ms <dungeon666master@protonmail.com>
2022-07-20 00:50:09 +0200

    get_config args kwargs

--- pytonlib/client.py
+++ pytonlib/client.py
@@ -601,7 +601,7 @@ class TonlibClient:
         }
         return await self.tonlib_wrapper.execute(request)
 
-    async def get_config(self, config_id: int, seqno: int):
+    async def get_config(self, config_id: int, seqno: int, *args, **kwargs):
         wc, shard = -1, -9223372036854775808
         fullblock = await self.lookup_block(wc, shard, seqno)
         request = {
